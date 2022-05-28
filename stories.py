5099b6dc7a93d43c18a516f333e383df92249453
AdBanacho <48532170+AdBanacho@users.noreply.github.com>
2022-05-28 12:03:33 +0200

    Update stories.py

--- stories.py
+++ stories.py
@@ -9,14 +9,14 @@ class Interface:
         self.gpt = GPT
 
     def run(self):
-        st.markdown("Chcesz uslyszec interesujace historie o Twojej rodzinie?")
+        st.header("Chcesz uslyszec interesujace historie o Twojej rodzinie?")
         self.topic = st.select_slider("", ["Piotr", "Igor", "Karolina"], "Igor")
         if st.button("Opowiadaj!", key=4):
             if self.topic == "Piotr":
                 story = self.gpt.gpt_3(["driving a car", "boring", "my husband Piotr"], 1.0, p.generate_prompt_story, 256)
                 st.write(self.gpt.gpt_3(story + "so boring...", 0.7, p.generate_prompt_translate, 512))
             elif self.topic == "Igor":
-                story = self.gpt.gpt_3(["climbing on Mount Everest", "amazing", "my son Igor"], 1.0, p.generate_prompt_story, 256)
+                story = self.gpt.gpt_3(["competitive bike ride on the grand canyon", "amazing", "my son Igor"], 1.0, p.generate_prompt_story, 256)
                 st.write(self.gpt.gpt_3(story, 0.7, p.generate_prompt_translate, 512))
             elif self.topic == "Karolina":
                 story = self.gpt.gpt_3(["making a spaghetti", "funny", "my daughter Karolina"], 1.0, p.generate_prompt_story, 256)
